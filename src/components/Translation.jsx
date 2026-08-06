0712e2256fa201db74e1cd3b961ea5f2d223975f
Kauã Thalison <thaleskaua66@gmail.com>
2026-08-06 20:40:45 -0300

    Starting login/register development

--- src/components/Translation.jsx
+++ src/components/Translation.jsx
@@ -10,7 +10,7 @@ const Translation = ({
   linkMobile,
 }) => {
   return (
-    <div className="bg-gray-800 w-64 rounded-md relative transition-transform duration-300 hover:scale-110">
+    <div className="flex flex-col bg-gray-800 w-64 rounded-md relative transition-transform duration-300 hover:scale-110">
       {banner !== "" && (
         <img
           src={banner}
@@ -28,10 +28,10 @@ const Translation = ({
       <img
         src={img}
         alt={"Imagem de " + name}
-        className="absolute w-16 left-4 -mt-12 bg-gray-800 p-1 rounded-lg"
+        className="absolute w-16 left-4 top-20 bg-gray-800 p-1 rounded-lg"
       />
       {/* INFO DIV */}
-      <div className="flex flex-col gap-3 mt-3 p-3 min-h-42 justify-between">
+      <div className="flex flex-1 flex-col gap-3 mt-3 p-3 min-h-42 justify-between">
         <p className="font-bold">{name}</p>
         <p className="text-sm">{description}</p>
         <div className="flex gap-3">
